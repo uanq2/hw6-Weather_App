6fc2fe8d84543b2503112bd91216d2a6a893d0d4
Uriel Nunez <uanq22@hotmail.com>
2021-05-22 12:42:28 -0500

    get the api working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // assign global variables /////////////////////////////////////////////////////
 let apiKey = '345dea590c25774e262329034275a689';
-// HMTL IDS
+
+// HTML IDS
 // city
 // temp
 // wind
@@ -9,6 +10,7 @@ let apiKey = '345dea590c25774e262329034275a689';
 // five day container
 // cities history
 let cities = [];
+
 // functions //////////////////////////////////////////////////////////////////
 // init
 function init() {
@@ -24,14 +26,52 @@ function init() {
     }
     console.log('no data');
 }
+
+function makePastBtn(cities) {
+    for (i = 0; i < cities.length; i++) {
+        $("#pastCity").prepend(
+            `<button data-past=${cities[i]} class="button is-light">${cities[i]}</button>`
+        );
+    };
+};
+
+function storeCity() {
+    let pastCities = cities;
+    localStorage.setItem("cities", JSON.stringify(pastCities));
+};
+
 // getWeather
 function getWeather() {
-    fetch('api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${345dea590c25774e262329034275a689}')
-        .then(response => response.json())
+    let city = document.getElementById("inputSearch").value || "chicago";
+    let endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+    fetch(endpoint)
+
+        .then((res) => res.json())
         .then(data => {
-            let getWeather = data['weather'][0]['main'];
-        })
+            let long = data.city.coord.lon;
+            let latt = data.city.coord.lat;
+            weatherApi(long, latt);
+            document.getElementById("city").textContent = data.city.name;
+            console.log(data);
+        });
 }
+
+function weatherApi(lon, lat) {
+    let endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
+    fetch(endpoint)
+
+        .then((res) => res.json())
+        .then(data => {
+            document.getElementById("temp").textContent = data.current.temp;
+            document.getElementById("wind").textContent = data.current.wind_speed;
+            document.getElementById("humidity").textContent = data.current.humidity;
+            document.getElementById("index").textContent = data.current.uvi;
+            console.log(data);
+        });
+
+}
+getWeather();
+
 // parameter: value of search box (city name)
 // call the weather api with the city name to get the coordinates (lat, lon)
 // find the lat and lon within the data and set them as variables
@@ -47,3 +87,5 @@ function getWeather() {
 init();
 // click search button - call the api and get our cream filling
 // click on past city button (class) - just call the getWeather function with the label of the button
+
+$("#searchBtn").on("click", () => getWeather());
