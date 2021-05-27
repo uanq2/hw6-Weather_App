59b4d58e6a0c728070ce89a511e92573d076f381
Uriel Nunez <uanq22@hotmail.com>
2021-05-27 00:49:51 -0500

    the cards are finally working, styled, past searches are showing, icons are also displaying

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,96 +1,135 @@
-// assign global variables /////////////////////////////////////////////////////
-let apiKey = '345dea590c25774e262329034275a689';
-
-// HTML IDS
-// city
-// temp
-// wind
-// humidity
-// uv index - set class
-// five day container
-// cities history
+let city = "";
+let apiKey = "345dea590c25774e262329034275a689";
+let searchCity = $("#search-city");
+let searchButton = $("#search-button");
+let currentCity = $("#current-city");
+let currentTemperature = $("#temperature");
+let currentHumidty = $("#humidity");
+let currentWSpeed = $("#wind-speed");
+let currentUvindex = $("#uv-index");
 let cities = [];
 
-// functions //////////////////////////////////////////////////////////////////
-// init
-function init() {
-    // check local storage for the key (cities) if present
-    let citiesStorage = localStorage.getItem('cities');
-    if (citiesStorage) {
-        // loop through local storage and create buttons with the button label as the city
-        cities = JSON.parse(citiesStorage);
-        console.log(cities);
-        cities.forEach(city => {
-            // make and append a button to the left panel
-        })
+function find(c) {
+    for (let i = 0; i < cities.length; i++) {
+        if (c.toUpperCase() === cities[i]) {
+            return -1;
+        }
     }
-    console.log('no data');
+    return 1;
 }
 
-function checkValue(e) {
-    console.log();
-    let selectedCity = e.target.getAttribute("data-past");
-    getLatLon(selectedCity);
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
+        currentWeather(city);
+    }
 }
 
-function makePastBtn(cities) {
-    for (i = 0; i < cities.length; i++) {
-        $("#pastCity").prepend(
-            `<button data-past=${cities[i]} class="button is-light">${cities[i]}</button>`
-        );
-    };
-};
+function currentWeather(city) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        let weathericon = response.weather[0].icon;
+        let iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+        let date = new Date(response.dt * 1000).toLocaleDateString();
+        $(currentCity).html(response.name + "(" + date + ")" + "<img src=" + iconurl + ">");
 
-function storeCity() {
-    let pastCities = cities;
-    localStorage.setItem("cities", JSON.stringify(pastCities));
-};
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2) + "&#8457");
+        $(currentHumidty).html(response.main.humidity + "%");
+        let ws = response.wind.speed;
+        let windsmph = (ws * 2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph + "MPH");
+        UVIndex(response.coord.lon, response.coord.lat);
+        forecast(response.id);
+        if (response.cod == 200) {
+            cities = JSON.parse(localStorage.getItem("cityname"));
+            if (cities == null) {
+                cities = [];
+                cities.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname", JSON.stringify(cities));
+                addToList(city);
+            }
+            else {
+                if (find(city) > 0) {
+                    cities.push(city.toUpperCase());
+                    localStorage.setItem("cityname", JSON.stringify(cities));
+                    addToList(city);
+                }
+            }
+        }
 
-// getWeather
-function getWeather() {
-    let city = document.getElementById("inputSearch").value || "chicago";
-    let endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
-    fetch(endpoint)
-
-        .then((res) => res.json())
-        .then(data => {
-            let long = data.city.coord.lon;
-            let latt = data.city.coord.lat;
-            weatherApi(long, latt);
-            document.getElementById("city").textContent = data.city.name;
-            console.log(data);
-        });
+    });
 }
 
-function weatherApi(lon, lat) {
-    let endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
-    fetch(endpoint)
+function UVIndex(ln, lt) {
+    let uvqURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lt + "&lon=" + ln;
+    $.ajax({
+        url: uvqURL,
+        method: "GET"
+    }).then(function (response) {
+        $(currentUvindex).html(response.value);
+    });
+}
 
-        .then((res) => res.json())
-        .then(data => {
-            document.getElementById("temp").textContent = data.current.temp;
-            document.getElementById("wind").textContent = data.current.wind_speed;
-            document.getElementById("humidity").textContent = data.current.humidity;
-            document.getElementById("index").textContent = data.current.uvi;
-            console.log(data);
-        });
+function forecast(cityid) {
+    let dayover = false;
+    let queryforcastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
+    $.ajax({
+        url: queryforcastURL,
+        method: "GET"
+    }).then(function (response) {
+
+        for (i = 0; i < 5; i++) {
+            let date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+            let iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+            let iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            let tempK = response.list[((i + 1) * 8) - 1].main.temp;
+            let tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(2);
+            let humidity = response.list[((i + 1) * 8) - 1].main.humidity;
+
+            $("#fDate" + i).html(date);
+            $("#fImg" + i).html("<img src=" + iconurl + ">");
+            $("#fTemp" + i).html(tempF + " " + "&#8457");
+            $("#fHumidity" + i).html(humidity + "%");
+        }
+    });
+}
+
+function addToList(c) {
+    let listEl = $("<li>" + c.toUpperCase() + "</li>");
+    $(listEl).attr("class", "list-group-item");
+    $(listEl).attr("data-value", c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+
+function invokePastSearch(event) {
+    let liEl = event.target;
+    if (event.target.matches("li")) {
+        city = liEl.textContent.trim();
+        currentWeather(city);
+    }
 
 }
-getWeather();
 
-// parameter: value of search box (city name)
-// call the weather api with the city name to get the coordinates (lat, lon)
-// find the lat and lon within the data and set them as variables
-// in the then of the call above, use the lat and lon to get current weather and future
-// in the then of the call above, i find the data i need for the top card on the right (city, date, temp, wind, humidity, uv index)
-// RENDER FUNCTION if uv index greater than some value, set the class
-// RENDER FUNCTION for the 5 day forecast i want to loop through array of daily data and dynamically create a card and append it to the website
-// each card will have date, icon for condition, temp, wind, humidity
-// save to local storage the city the user just searched,
-// check local storage for that city, don't add if already there
-// events ////////////////////////////////////////////////////////////////////
-// init - check local storage
-$("#searchBtn").on("click", () => getWeather());
-init();
-// click search button - call the api and get our cream filling
-// click on past city button (class) - just call the getWeather function with the label of the button
+function loadlastCity() {
+    $("ul").empty();
+    let cities = JSON.parse(localStorage.getItem("cityname"));
+    if (cities !== null) {
+        cities = JSON.parse(localStorage.getItem("cityname"));
+        for (i = 0; i < cities.length; i++) {
+            addToList(cities[i]);
+        }
+        city = cities[i - 1];
+        currentWeather(city);
+    }
+
+}
+
+$("#search-button").on("click", displayWeather);
+$(document).on("click", invokePastSearch);
+$(window).on("load", loadlastCity);
