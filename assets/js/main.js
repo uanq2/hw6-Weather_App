4c0084fdbf03701eca6b3af681bd211bc4de214e
Uriel Nunez <uanq22@hotmail.com>
2021-05-28 21:57:21 -0500

    cleaned out HTML, CSS and JS files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ let searchCity = $("#search-city");
 let searchButton = $("#search-button");
 let currentCity = $("#current-city");
 let currentTemperature = $("#temperature");
-let currentHumidty = $("#humidity");
+let currentHumidity = $("#humidity");
 let currentWSpeed = $("#wind-speed");
-let currentUvindex = $("#uv-index");
+let currentUvIndex = $("#uv-index");
 let cities = [];
 
 function find(c) {
     for (let i = 0; i < cities.length; i++) {
-        if (c.toUpperCase() === cities[i]) {
+        if (c.toLowerCase() === cities[i]) {
             return -1;
         }
     }
@@ -22,42 +22,42 @@ function displayWeather(event) {
     event.preventDefault();
     if (searchCity.val().trim() !== "") {
         city = searchCity.val().trim();
-        currentWeather(city);
+        getWeather(city);
     }
 }
 
-function currentWeather(city) {
+function getWeather(city) {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        let weathericon = response.weather[0].icon;
-        let iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+        let weatherIcon = response.weather[0].icon;
+        let iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
         let date = new Date(response.dt * 1000).toLocaleDateString();
-        $(currentCity).html(response.name + "(" + date + ")" + "<img src=" + iconurl + ">");
+        $(currentCity).html(response.name + "(" + date + ")" + "<img src=" + iconUrl + ">");
 
         let tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $(currentTemperature).html((tempF).toFixed(2) + "&#8457");
-        $(currentHumidty).html(response.main.humidity + "%");
+        $(currentHumidity).html(response.main.humidity + "%");
         let ws = response.wind.speed;
-        let windsmph = (ws * 2.237).toFixed(1);
-        $(currentWSpeed).html(windsmph + "MPH");
+        let windsMPH = (ws * 2.237).toFixed(1);
+        $(currentWSpeed).html(windsMPH + "MPH");
         UVIndex(response.coord.lon, response.coord.lat);
         forecast(response.id);
         if (response.cod == 200) {
-            cities = JSON.parse(localStorage.getItem("cityname"));
+            cities = JSON.parse(localStorage.getItem("cityName"));
             if (cities == null) {
                 cities = [];
-                cities.push(city.toUpperCase()
+                cities.push(city.toLowerCase()
                 );
-                localStorage.setItem("cityname", JSON.stringify(cities));
+                localStorage.setItem("cityName", JSON.stringify(cities));
                 addToList(city);
             }
             else {
                 if (find(city) > 0) {
-                    cities.push(city.toUpperCase());
-                    localStorage.setItem("cityname", JSON.stringify(cities));
+                    cities.push(city.toLowerCase());
+                    localStorage.setItem("cityName", JSON.stringify(cities));
                     addToList(city);
                 }
             }
@@ -72,28 +72,28 @@ function UVIndex(ln, lt) {
         url: uvqURL,
         method: "GET"
     }).then(function (response) {
-        $(currentUvindex).html(response.value);
+        $(currentUvIndex).html(response.value);
     });
 }
 
-function forecast(cityid) {
-    let dayover = false;
-    let queryforcastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
+function forecast(cityId) {
+    let dayOver = false;
+    let queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&appid=" + apiKey;
     $.ajax({
-        url: queryforcastURL,
+        url: queryForecastURL,
         method: "GET"
     }).then(function (response) {
 
         for (i = 0; i < 5; i++) {
             let date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
             let iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
-            let iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            let iconUrl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
             let tempK = response.list[((i + 1) * 8) - 1].main.temp;
             let tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(2);
             let humidity = response.list[((i + 1) * 8) - 1].main.humidity;
 
             $("#fDate" + i).html(date);
-            $("#fImg" + i).html("<img src=" + iconurl + ">");
+            $("#fImg" + i).html("<img src=" + iconUrl + ">");
             $("#fTemp" + i).html(tempF + " " + "&#8457");
             $("#fHumidity" + i).html(humidity + "%");
         }
@@ -101,9 +101,9 @@ function forecast(cityid) {
 }
 
 function addToList(c) {
-    let listEl = $("<li>" + c.toUpperCase() + "</li>");
+    let listEl = $("<li>" + c.toLowerCase() + "</li>");
     $(listEl).attr("class", "list-group-item");
-    $(listEl).attr("data-value", c.toUpperCase());
+    $(listEl).attr("data-value", c.toLowerCase());
     $(".list-group").append(listEl);
 }
 
@@ -111,25 +111,25 @@ function invokePastSearch(event) {
     let liEl = event.target;
     if (event.target.matches("li")) {
         city = liEl.textContent.trim();
-        currentWeather(city);
+        getWeather(city);
     }
 
 }
 
-function loadlastCity() {
+function loadLastCity() {
     $("ul").empty();
-    let cities = JSON.parse(localStorage.getItem("cityname"));
+    let cities = JSON.parse(localStorage.getItem("cityName"));
     if (cities !== null) {
-        cities = JSON.parse(localStorage.getItem("cityname"));
+        cities = JSON.parse(localStorage.getItem("cityName"));
         for (i = 0; i < cities.length; i++) {
             addToList(cities[i]);
         }
         city = cities[i - 1];
-        currentWeather(city);
+        getWeather(city);
     }
 
 }
 
 $("#search-button").on("click", displayWeather);
 $(document).on("click", invokePastSearch);
-$(window).on("load", loadlastCity);
+$(window).on("load", loadLastCity);
